bd1277e79d4308764586719ffe434aefef2d7c97
Gbolagade winner <gbolagadewinner@gmail.com>
2021-09-06 15:25:15 -0400

    Procfile

--- resources/views/backend/admin/view_admin.blade.php
+++ resources/views/backend/admin/view_admin.blade.php
@@ -39,7 +39,7 @@
 								<td>{{$user->email}}</td>
 								<td>
 									<a href="{{ url('admin/edit/'.$user->id) }}" class="btn btn-info">Edit</a>
-									<a href="{{ url('admin/delete/'.$user->id) }}" class="btn btn-danger">Delete</a>
+									<a href="{{ url('admin/delete/'.$user->id) }}" class="btn btn-danger" id="delete">Delete</a>
 								</td>
 							</tr>
 							@endforeach
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -46,6 +46,14 @@
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> 
 
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script type="text/javascript">
+    $(function(){
+      $(document).on('click','#delete', function(){
+
+      });
+     });
+</script>
  <script>
   @if(Session::has('message'))
     var type = "{{ Session::get('alert-type', 'info') }}";
